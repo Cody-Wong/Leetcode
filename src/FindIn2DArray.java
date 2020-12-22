b60c8dbbc8aa8faf89887327ebda47188db8a741
hyx <735303871@qq.com>
2020-12-22 22:48:38 +0800

    二维数组中的查找

--- src/FindIn2DArray.java
+++ src/FindIn2DArray.java
@@ -1,13 +1,28 @@
 /**
  * 二维数组中的查找
- *
+ * 线性查找法，从右上角查找到左下角，时间复杂度O(m+n) m n分别为matrix的行列数
  * @Author Cody
  * @Date 2020/12/22
  */
 public class FindIn2DArray {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        //避免空二维数组导致数组越界
+        if(matrix.length == 0){
+            return false;
+        }
+        int row = 0;
+        int column = matrix[0].length -1;
+
+        while(row <= matrix.length -1 && column >= 0){
+            if(target > matrix[row][column]){
+                row++;
+            }else if(target < matrix[row][column]){
+                column--;
+            }else{
+                return true;
+            }
+        }
 
         return false;
     }
-
 }
