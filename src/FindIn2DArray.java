c4052c4ebb5c652ad73ad30b4dd073c33c1fac77
hyx <735303871@qq.com>
2020-12-22 20:41:03 +0800

    最小栈

--- src/FindIn2DArray.java
+++ src/FindIn2DArray.java
@@ -1,12 +1,12 @@
 /**
- * 类功能描述
+ * 二维数组中的查找
  *
- * @Author hyx
+ * @Author Cody
  * @Date 2020/12/22
  */
 public class FindIn2DArray {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
-        int
+
         return false;
     }
 
---
+++ src/MinStack.java
@@ -0,0 +1,58 @@
+/**
+ * 包含min函数的栈
+ * 除了原始栈外还要维护一个最小栈，记录每次push pop后的最小值
+ * @Author hyx
+ * @Date 2020/12/22
+ */
+class MinStack {
+    private int[] mainArray;
+    private int[] minArray;
+    private int top = -1; //不可为0
+    private int stackSize = 10000;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        mainArray = new int[stackSize];
+        minArray = new int[stackSize];
+    }
+
+    public void push(int x) {
+        top++;
+        mainArray[top] = x;
+        //栈为空时
+        if(top == 0){
+            minArray[top] = x;
+        }else{
+            //和之前的最小值比较
+            if(x < minArray[top-1]){
+                minArray[top] = x;
+            }else{
+                minArray[top] = minArray[top-1];
+            }
+        }
+    }
+
+    public void pop() {
+        //当又一次为空栈时，top又再一次变为-1
+        if(top >= 0){
+            top--;
+        }
+    }
+
+    public int top() {
+        return mainArray[top];
+    }
+
+    public int min() {
+        return minArray[top];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.min();
+ */
